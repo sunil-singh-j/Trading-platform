1914b9d92b5d75ff046a0a3324c8682bf5a385a7
sunil-singh-j <sunilsinghrjjaat@gmail.com>
2025-04-17 22:18:32 +0530

    basic setup done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button';
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Button>dfdfdf</Button>
+    </>
+  )
+}
+
+export default App
