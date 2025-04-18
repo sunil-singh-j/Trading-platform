a3bc1b7c4a6a31b08abe589f326b99eec5fb00af
sunil-singh-j <sunilsinghrjjaat@gmail.com>
2025-04-18 18:52:12 +0530

    updatedhomepage

--- my-app/src/page/Home/Home.jsx
+++ my-app/src/page/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+import Assettable from './Assettable'
 
 function Home() {
+    const [category,setcategory]=useState("all")
+    const handleCategory=(value)=>{
+        setcategory(value)
+    }
   return (
-    <div>
-      Home
+    <div className='relative'>
+        <div className='lg:flex'>
+            <div className='p-3 flex items-center gap-4'>
+                <Button onClick={()=>handleCategory("all")} variant={category=="all"?"default":"outline"} className="rounded-full">All</Button>
+                <Button onClick={()=>handleCategory("top50")} variant={category=="top50"?"default":"outline"} className="rounded-full">Top 50</Button>
+                <Button onClick={()=>handleCategory("topGainers")} variant={category=="topGainers"?"default":"outline"} className="rounded-full">Top Gainers</Button>
+                <Button onClick={()=>handleCategory("topLosers")} variant={category=="topLosers"?"default":"outline"} className="rounded-full">Top Losers</Button>
+            </div>
+        </div>
+        <Assettable></Assettable>
     </div>
   )
 }
